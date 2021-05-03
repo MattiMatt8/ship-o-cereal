cb9466a3cfffd1609f69501cff519ec2aa9957a1
valdimarp20 <valdimarp20@ru.is>
2021-05-03 19:44:03 +0000

    added indexing for homepage

--- ShipOCereal/settings.py
+++ ShipOCereal/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
