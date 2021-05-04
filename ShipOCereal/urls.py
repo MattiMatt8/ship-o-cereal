cdc7930dcc61377ac8b27b10fe6dc2396e120898
valdimarp20 <valdimarp20@ru.is>
2021-05-04 21:41:08 +0000

    homePageView

--- ShipOCereal/urls.py
+++ ShipOCereal/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.HomePageView.as_view(), name="index"),
     path("", include("users.urls"), name="users"),
     path("orders/", include("orders.urls"), name="orders"),
     path("products/", include("products.urls"), name="products"),
