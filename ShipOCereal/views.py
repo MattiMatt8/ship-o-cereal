023f95c39bc30eeb55d0314efa103e7ab9e0e726
PeturSteinn <petur.steinn@icloud.com>
2021-05-04 02:13:40 +0000

    Dynamic front page

--- ShipOCereal/views.py
+++ ShipOCereal/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
-    return render(request, 'views/index.html')
+    # TODO: Send in a context of top 5 products in cereals, bowls and merch (id, title, price, image)
+
+    # Wack temporary code below...
+    categories = ('Cereal', 'Bowls', 'Merchandise')
+    context = {
+        'categories': {
+            category: Product.objects.filter(category__contains=category)[:5] for category in categories
+        }
+    }
+    print(context)
+    #context['categories'] = categories
+
+    return render(request, 'views/index.html', context=context)
--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from products.models import Product
 
 
 def index(response):
     return HttpResponse("yes")
+
+def product_details(response, id):
+    return render(response, 'views/product_details.html', {
+        'product': get_object_or_404(Product, pk=id)
+    })
