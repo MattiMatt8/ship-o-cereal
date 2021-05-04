e3691c8eb20b6cc35139d117b652a104c8b9be87
valdimarp20 <valdimarp20@ru.is>
2021-05-04 13:55:28 +0000

    temp fix for categories

--- ShipOCereal/views.py
+++ ShipOCereal/views.py
@@ -5,13 +5,15 @@ def index(request):
     # TODO: Send in a context of top 5 products in cereals, bowls and merch (id, title, price, image)
 
     # Wack temporary code below...
-    categories = ('Cereal', 'Bowls', 'Merchandise')
+    # categories = ('Cereal', 'Bowls', 'Merchandise')
+    # TODO: Fix this. This is only a temporary solution for the DB changes
     context = {
         'categories': {
-            category: Product.objects.filter(category__contains=category)[:5] for category in categories
+            'Cereal', 'Bowls', 'Merchandise'
+            # category: Product.objects.filter(category__contains=category)[:5] for category in categories
         }
     }
-    print(context)
-    #context['categories'] = categories
+    # print(context)
+    # context['categories'] = categories
 
     return render(request, 'views/index.html', context=context)
