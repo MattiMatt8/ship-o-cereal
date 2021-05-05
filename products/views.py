cfdc6768f27c3fd7f73dd1101eadce5b0ba77867
valdimarp20 <valdimarp20@ru.is>
2021-05-05 13:15:51 +0000

    Change CBV to use set_all method

--- products/views.py
+++ products/views.py
@@ -3,42 +3,27 @@ from django.views.generic import ListView, DetailView, View
 from .models import Category, Product
 
 
-
 class ProductCategory(View):
     def get(self, *args, **kwargs):
 
         context = {}
         try:
-            category = Category.objects.get(name=kwargs["category_name"])
-            context["category"] = category
 
-            category_products = Product.objects.filter(category_id=category.id)
+            # category_products = Product.objects.filter(category_id=category.id)
+            category = Category.objects.get(name=kwargs["category_name"])
+            category_products = category.product_set.all()
+
+            context["category"] = category
             context["products"] = category_products
         except Category.DoesNotExist:
             pass
 
-        return render(self.request, 'category.html', context)
-
-# def product_category(req, category_name):
-#     context = {}
-#     category_name = category_name
-#
-#     try:
-#         category = Category.objects.get(name=category_name)
-#         context["category"] = category
-#
-#         category_products = Product.objects.filter(category_id=category.id)
-#         context["products"] = category_products
-#
-#     except Category.DoesNotExist:
-#         pass
-#
-#     return render(req, 'category.html', context)
+        return render(self.request, 'category/category.html', context)
 
 
 def product_details(response, id):
     return render(
         response,
         "product_details.html",
-        {"product": get_object_or_404(Product, pk=id), "amount_list": range(1,11)},
+        {"product": get_object_or_404(Product, pk=id), "amount_list": range(1, 11)},
     )
