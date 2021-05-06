f182855e2cf4de3bf51036e1686c0ef605aabfe8
valdimarp20 <valdimarp20@ru.is>
2021-05-06 15:24:10 +0000

    brand filter

--- products/views.py
+++ products/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django_filters.views import FilterView
 from .models import Category, Product
 from .filters import ProductFilter
 
 
-class ProductsInCategoryListView(ListView):
-    template_name = "category/category.html"
-    paginate_by = 10
+class FilteredListView(FilterView):
+    filterset_class = None
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = self.filterset_class(self.request.GET, queryset=queryset)
+        return self.filterset.qs.distinct()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["filter"] = ProductFilter(self.request.GET, queryset=self.get_queryset())
+        context['filterset'] = self.filterset
         return context
+
+
+class ProductsInCategoryListView(FilteredListView):
+    paginate_by = 10
+    filterset_class = ProductFilter
+    context_object_name = "products"
+    template_name = "category/category.html"
 
     def get_queryset(self):
         category = get_object_or_404(Category, name=self.kwargs["category_name"])
         return category.product_set.all()
-
-#class ProductSearch
 
 
 def product_details(response, id):
