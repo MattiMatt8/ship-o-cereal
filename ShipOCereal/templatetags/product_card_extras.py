413382a3e5a6274ee752a7e7e4b16fa6cb681bb3
PeturSteinn <petur.steinn@icloud.com>
2021-05-08 00:52:06 +0000

    What

--- ShipOCereal/templatetags/product_card_extras.py
+++ ShipOCereal/templatetags/product_card_extras.py
@@ -3,7 +3,4 @@ from django.template.defaulttags import register
 
 @register.filter
 def get_item(dictionary, key):
-    print("yeet")
-    print(dictionary)
-    print("yote")
     return dictionary.get(str(key))
