1fa385e7b7a01ebc50a2daaa10085b7c7a58eb12
PeturSteinn <petur.steinn@icloud.com>
2021-05-10 17:18:07 +0000

    Search stuff

--- static/js/main.js
+++ static/js/main.js
@@ -4,107 +4,107 @@ let cartTotal = Number(cartTotalElement.innerText);
 
 // Function getCookie from: https://docs.djangoproject.com/en/3.0/ref/csrf/#ajax
 function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === name + "=") {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
-  }
-  return cookieValue;
+    return cookieValue;
 }
 
 const CSRF_TOKEN = getCookie("csrftoken");
 
 function updateCart(id, quantity, callback = undefined, in_cart = false) {
-  axios
-    .post(
-      CART_URL + id + "/",
-      { quantity: quantity, in_cart: in_cart },
-      { headers: { "X-CSRFToken": CSRF_TOKEN } }
-    )
-    .then((response) => {
-      cartUpdateTotal(quantity, response.data.old_quantity);
-      if (callback) {
-        callback();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      if (callback) {
-        callback(error);
-      }
-    });
+    axios
+        .post(
+            CART_URL + id + "/",
+            {quantity: quantity, in_cart: in_cart},
+            {headers: {"X-CSRFToken": CSRF_TOKEN}}
+        )
+        .then((response) => {
+            cartUpdateTotal(quantity, response.data.old_quantity);
+            if (callback) {
+                callback();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            if (callback) {
+                callback(error);
+            }
+        });
 }
 
 function deleteFromCart(id, callback = undefined, in_cart = false) {
-  axios
-    .post(
-      CART_URL + id + "/delete/",
-      { in_cart: in_cart },
-      { headers: { "X-CSRFToken": CSRF_TOKEN } }
-    )
-    .then((response) => {
-      cartDeleteFromTotal(response.data.quantity);
-      if (callback) {
-        callback();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      if (callback) {
-        callback(error);
-      }
-    });
+    axios
+        .post(
+            CART_URL + id + "/delete/",
+            {in_cart: in_cart},
+            {headers: {"X-CSRFToken": CSRF_TOKEN}}
+        )
+        .then((response) => {
+            cartDeleteFromTotal(response.data.quantity);
+            if (callback) {
+                callback();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            if (callback) {
+                callback(error);
+            }
+        });
 }
 
 function newSearch(search, callback = undefined) {
-  axios
-    .post(
-      "/profile/search/new/",
-      { search: search },
-      { headers: { "X-CSRFToken": CSRF_TOKEN } }
-    )
-    .then((response) => {
-      if (callback) {
-        callback();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    axios
+        .post(
+            "/profile/search/new/",
+            {search: search},
+            {headers: {"X-CSRFToken": CSRF_TOKEN}}
+        )
+        .then((response) => {
+            if (callback) {
+                callback();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 function deleteSearch(id, callback = undefined) {
-  axios
-    .post(
-      "/profile/search/" + id + "/delete/",
-      null,
-      { headers: { "X-CSRFToken": CSRF_TOKEN } }
-    )
-    .then((response) => {
-      if (callback) {
-        callback();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    axios
+        .post(
+            "/profile/search/" + id + "/delete/",
+            null,
+            {headers: {"X-CSRFToken": CSRF_TOKEN}}
+        )
+        .then((response) => {
+            if (callback) {
+                callback();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 function cartUpdateTotal(quantity, oldQuantity) {
-  cartTotal += quantity - Number(oldQuantity);
-  cartTotalElement.innerText = cartTotal;
+    cartTotal += quantity - Number(oldQuantity);
+    cartTotalElement.innerText = cartTotal;
 }
 
 function cartDeleteFromTotal(quantity = 1) {
-  cartTotal -= quantity;
-  cartTotalElement.innerText = cartTotal;
+    cartTotal -= quantity;
+    cartTotalElement.innerText = cartTotal;
 }
 
 
@@ -115,100 +115,119 @@ const searchDropdown = document.getElementById("search-dropdown");
 let isHoveringDropdown = false;
 
 searchInput.addEventListener("focusin", (e) => {
-  searchDropdown.classList.add("animate-searchDropdownOpen");
-  searchDropdown.classList.remove("animate-searchDropdownClose");
+    searchDropdown.classList.add("animate-searchDropdownOpen");
+    searchDropdown.classList.remove("animate-searchDropdownClose");
 });
 
 searchInput.addEventListener("focusout", (e) => {
-  if (!isHoveringDropdown) {
-    searchDropdown.classList.remove("animate-searchDropdownOpen");
-    searchDropdown.classList.add("animate-searchDropdownClose");
-  }
+    if (!isHoveringDropdown) {
+        searchDropdown.classList.remove("animate-searchDropdownOpen");
+        searchDropdown.classList.add("animate-searchDropdownClose");
+    }
 });
 
 searchDropdown.addEventListener("mouseover", (e) => {
-  isHoveringDropdown = true;
+    isHoveringDropdown = true;
 });
 
 searchDropdown.addEventListener("mouseleave", (e) => {
-  isHoveringDropdown = false;
-  if (!(document.activeElement === searchInput)) {
-    searchDropdown.classList.remove("animate-searchDropdownOpen");
-    searchDropdown.classList.add("animate-searchDropdownClose");
-  }
+    isHoveringDropdown = false;
+    if (!(document.activeElement === searchInput)) {
+        searchDropdown.classList.remove("animate-searchDropdownOpen");
+        searchDropdown.classList.add("animate-searchDropdownClose");
+    }
 });
 
 const renderNotification = (object, type) => {
-  if (type === "error") {
-    const errorNotification = document.getElementById("error-notification");
-    const messageSpan = document.getElementById("error-notification-message");
+    if (type === "error") {
+        const errorNotification = document.getElementById("error-notification");
+        const messageSpan = document.getElementById("error-notification-message");
 
-    messageSpan.textContent = object.response.data.message;
+        messageSpan.textContent = object.response.data.message;
 
-    errorNotification.classList.add("right-6");
-    errorNotification.classList.remove("-right-96");
+        errorNotification.classList.add("right-6");
+        errorNotification.classList.remove("-right-96");
 
-    setTimeout(() => {
-      errorNotification.classList.remove("right-6");
-      errorNotification.classList.add("-right-96");
-    }, 5000);
-  }
+        setTimeout(() => {
+            errorNotification.classList.remove("right-6");
+            errorNotification.classList.add("-right-96");
+        }, 5000);
+    }
 };
 
 // Input filter used for validation
 
 // Restricts input for the given textbox to the given inputFilter function.
 const setInputFilter = (textbox, inputFilter) => {
-  //["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
-  textbox.addEventListener("input", function (e) {
-    if (inputFilter(this.value) && this.value != 0) {
-      // If a number was an input, it's safe to make the request to the server
-      const callback = (error) => {
-        if (!error) {
-          // If success then mark the new number as an old number we can revert to if something fails.
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-          return;
-        } else if (this.hasOwnProperty("oldValue")) {
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        } else {
-          this.value = "";
+    //["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+    textbox.addEventListener("input", function (e) {
+        if (inputFilter(this.value) && this.value != 0) {
+            // If a number was an input, it's safe to make the request to the server
+            const callback = (error) => {
+                if (!error) {
+                    // If success then mark the new number as an old number we can revert to if something fails.
+                    this.oldValue = this.value;
+                    this.oldSelectionStart = this.selectionStart;
+                    this.oldSelectionEnd = this.selectionEnd;
+                    return;
+                } else if (this.hasOwnProperty("oldValue")) {
+                    this.value = this.oldValue;
+                    this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                } else {
+                    this.value = "";
+                }
+                // TODO: Display error notification
+                renderNotification(error, "error");
+            };
+
+            const id = e.target.dataset.productId;
+            updateCart(id, Number(e.target.value), callback);
+            return;
         }
-        // TODO: Display error notification
-        renderNotification(error, "error");
-      };
 
-      const id = e.target.dataset.productId;
-      updateCart(id, Number(e.target.value), callback);
-      return;
-    }
-
-    if (this.hasOwnProperty("oldValue")) {
-      this.value = this.oldValue;
-      this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-    } else {
-      this.value = "";
-    }
-  });
+        if (this.hasOwnProperty("oldValue")) {
+            this.value = this.oldValue;
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+            this.value = "";
+        }
+    });
 };
 
 function searchListener() {
-  function handleForm(event) {
-    event.target.form.submit();
-  }
-  document
-    .getElementsByTagName("form")[0]
-    .addEventListener("submit", handleForm);
+    function handleForm(event) {
+        event.target.form.submit();
+    }
+
+    document
+        .getElementsByTagName("form")[0]
+        .addEventListener("submit", handleForm);
 }
 
 
 // Listen for delete search history
 const searchDeleteButtons = document.getElementsByClassName('search-delete-button');
 Array.from(searchDeleteButtons).forEach(button => {
-  button.addEventListener('click', e => {
-    const id = button.dataset.searchId;
-    console.log(`Gonna delete: ${id}`);
-  })
+    button.addEventListener('click', e => {
+        const id = button.dataset.searchId;
+
+        const callback = (error) => {
+            if (!error) {
+                const numOfListItems = button.parentElement.parentElement.childElementCount;
+                const numOfListItemsAfterDelete = numOfListItems - 1;
+
+                // If search history is empty this hould remove the whole dropdown box
+                if (numOfListItemsAfterDelete === 0) {
+                    button.parentElement.parentElement.parentElement.remove()
+                } else {
+                    // If search got deleted on the backend then remove the list item
+                    button.parentElement.remove()
+                }
+            }
+        }
+        
+
+
+
+    })
 })
