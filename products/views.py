29312102ede34fd76464e3201fd5a0298ff8d2fe
valdimarp20 <valdimarp20@ru.is>
2021-05-10 16:56:48 +0000

    basic docstring

--- products/views.py
+++ products/views.py
@@ -1,34 +1,49 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django_filters.views import FilterView
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from .filters import ProductFilter, ProductSearchFilter
 
 
 class FilteredListView(FilterView):
-    # Calls filters in filters.py
+    """
+    A parent class implementing ListView functionality with
+    added queryset filtering and pagination maintenance.
+    """
+
     def get_queryset(self):
+        """Returns a filtered queryset corresponding to query parameters."""
+
+        # Calling FilterView base implementation to get a queryset
+        # & initialising a filterset attribute
         queryset = super().get_queryset()
         self.filterset = self.filterset_class(self.request.GET, queryset=queryset)
 
-        return self.filterset.qs.distinct()
+        return self.filterset.qs.distinct()  # Filtered queryset
 
     def get_context_data(self, **kwargs):
+        """Returns context data with the filtered queryset & current query parameters."""
 
+        # Calling FilterView base implementation first to get a context
+        # & adding filterset to context for template rendering
         context = super().get_context_data(**kwargs)
         context["filterset"] = self.filterset
-        filters = ""
 
+        # Adding filter query parameters to maintain pagination
+        filters = ""
         for k, v in context["filterset"].data.items():
             if k != "page":
                 filters += f"&{k}={v}"
-        context["filters"] = filters
 
+        context["filters"] = filters
         return context
 
 
 class ProductsInCategoryListView(FilteredListView):
+    """
+    A class for listing and paginating products in a category, either
+    all products or products matching query parameters.
+    """
 
     paginate_by = 10
     filterset_class = ProductFilter
@@ -36,13 +51,16 @@ class ProductsInCategoryListView(FilteredListView):
     context_object_name = "products"
     template_name = "category/category.html"
 
-    def get_category(self):
-        return Category.objects.get(name=self.kwargs["category_name"])
+    def get_queryset(self):
+        """Returns a queryset with products in a category matching given query parameters."""
 
-    def get_queryset(self, **kwargs):
         category = self.get_category()
-
         return self.queryset.filter(category=category)
+
+    def get_category(self):
+        """Returns category object matching keyword argument 'category_name'."""
+
+        return Category.objects.get(name=self.kwargs["category_name"])
 
 
 class ProductSearch(FilteredListView):
