550281d4a58ed2cd65fc43d03b36dd8a872fbc0d
PeturSteinn <petur.steinn@icloud.com>
2021-05-10 11:38:12 +0000

    Label icons are now dynamic!

--- ShipOCereal/settings.py
+++ ShipOCereal/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
             ],
             "libraries": {
                 "product_card_extras": "ShipOCereal.templatetags.product_card_extras",
+                "general": "ShipOCereal.templatetags.general",
             },
         },
     },
