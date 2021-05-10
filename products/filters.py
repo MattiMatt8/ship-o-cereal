4199102d106be68177b904c27a8589412e803e7d
valdimarp20 <valdimarp20@ru.is>
2021-05-10 00:23:28 +0000

    filters.py

--- products/filters.py
+++ products/filters.py
@@ -1,17 +1,25 @@
 from django.utils.translation import gettext_lazy as _
 
 from .models import Brand
-from django_filters import (FilterSet, ModelMultipleChoiceFilter, ModelChoiceFilter, NumberFilter, OrderingFilter,
-                            CharFilter, UUIDFilter)
+from django_filters import (
+    FilterSet,
+    ModelMultipleChoiceFilter,
+    ModelChoiceFilter,
+    NumberFilter,
+    OrderingFilter,
+    CharFilter,
+    UUIDFilter,
+)
 from .models import Product, Label
 
 # Order by fields
 PRODUCT_ORDER_BY_FIELDS = (
-            ('price', 'Price: Low to High'),
-            ('-price', 'Price: High to Low'),
-            ('name', 'Name: A to Z'),
-            ('-name', 'Price: Z to A'),
-            ('-id', 'Newest arrivals'))
+    ("price", "Price: Low to High"),
+    ("-price", "Price: High to Low"),
+    ("name", "Name: A to Z"),
+    ("-name", "Price: Z to A"),
+    ("-id", "Newest arrivals"),
+)
 
 
 class ProductFilter(FilterSet):
@@ -21,20 +29,26 @@ class ProductFilter(FilterSet):
     name = CharFilter(field_name="name")
     id = NumberFilter(field_name="newest")
     labels = ModelMultipleChoiceFilter(queryset=Label.objects.all())
-    brand = ModelChoiceFilter(queryset=Brand.objects.all(), field_name="brand", empty_label=_("Select brand .."))
+    brand = ModelChoiceFilter(
+        queryset=Brand.objects.all(),
+        field_name="brand",
+        empty_label=_("Select brand .."),
+    )
 
     # Order by filter
     ordering = OrderingFilter(
-        label="Sort by",
-        choices=PRODUCT_ORDER_BY_FIELDS,
-        empty_label=_("Order by..")
+        label="Sort by", choices=PRODUCT_ORDER_BY_FIELDS, empty_label=_("Order by..")
     )
 
     class Meta:
         model = Product
-        fields = ['price', 'name']
+        fields = ["price", "name"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.form.fields["brand"].widget.attrs = {"class": "rounded border appearance-none border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base w-64 h-9 pl-3 pr-10"}
-        self.form.fields["ordering"].widget.attrs = {"class": "rounded border appearance-none border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base w-42 h-9 pl-3 pr-10"}
+        self.form.fields["brand"].widget.attrs = {
+            "class": "rounded border appearance-none border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base w-64 h-9 pl-3 pr-10"
+        }
+        self.form.fields["ordering"].widget.attrs = {
+            "class": "rounded border appearance-none border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base w-42 h-9 pl-3 pr-10"
+        }
