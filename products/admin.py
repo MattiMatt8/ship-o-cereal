10572d976c98fb67f14551c452fdd8d3fc9638eb
Odinnb <Odinnb20@ru.is>
2021-05-12 18:13:13 +0000

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-
+from django.db import models
 from products.models import Product
 
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'brand','active')
-    fields = ['name','brand','description','contents','weight','price', 'category','active','stock','percentage_off','discounted_price']
+    fields = ['name','brand','description','contents','weight','price', 'category','active','stock','percentage_off','discounted_price','']
     readonly_fields = ('id',)
     list_filter = ('brand',)
     list_editable = ('active',)
-    search_fields = ('id','name','brand')
+    search_fields = ('name','id','brand__name')
 
     def has_delete_permission(self, request, obj=None):
         return False
