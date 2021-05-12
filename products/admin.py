22a48c7cc81b442dd79143480635186feec39ec5
MattiMatt8 <momatthiasson@gmail.com>
2021-05-12 18:40:19 +0000

    Admin panel update for products

--- products/admin.py
+++ products/admin.py
@@ -15,5 +15,24 @@ class ProductAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+class ProductDiscountAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'brand','active')
+    fields = ['name','brand','description','contents','weight','price', 'category','active','stock','percentage_off','discounted_price']
+    readonly_fields = ('id',)
+    list_filter = ('brand',)
+    list_editable = ('active',)
+    search_fields = ('name','id','brand__name')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class ProductDiscount(Product):
+    class Meta:
+        proxy = True
+
+
+
 admin.site.register(Product, ProductAdmin)
+
+admin.site.register(ProductDiscount, ProductDiscountAdmin)
 
