bd39f1fcd9f0cfac6dbff77b161d672d48455a9e
valdimarp20 <valdimarp20@ru.is>
2021-05-12 21:54:00 +0000

    Added discount pricing for admin panel

--- products/admin.py
+++ products/admin.py
@@ -17,23 +17,43 @@ class ProductAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+
 class ProductDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id','name','percentage_off','discounted_price','new_price')
-    fields = ['name','brand','description','contents','weight','price', 'category','active','stock','percentage_off','discounted_price']
+    list_display = ('id', 'name', 'percentage_off', 'price', 'new_price')
+    list_display_links = None
+    fields = ['name', 'brand', 'description', 'contents', 'weight', 'price', 'category', 'active', 'stock',
+              'percentage_off', 'discounted_price']
     readonly_fields = ('id',)
     list_filter = ('brand',)
     list_editable = ('percentage_off',)
-    search_fields = ('name','id','brand__name')
+    search_fields = ('name', 'id', 'brand__name')
 
     def has_delete_permission(self, request, obj=None):
         return False
 
-    def new_price(self,obj):
+    def new_price(self, obj):
+        """Returns and sets the discounted price for a given product object."""
+
+        # If percentage_off exceeds 100, reset the price
+        if obj.percentage_off > 100:
+            obj.percentage_off = 0
+            obj.discounted_price = None
+            obj.save()
+
+            return obj.price
+
+        # Calculate the discounted price
         price = Fraction(obj.price)
         percentage_off = Fraction(obj.percentage_off)
         discount = (percentage_off / 100) * price
         final_price = Fraction(price - discount)
-        return float(Fraction(math.floor(final_price*100), 100))
+        new = float(Fraction(math.floor(final_price*100), 100))
+
+        # Save and set the discounted price
+        obj.discounted_price = new
+        obj.save()
+
+        return new
 
     def has_add_permission(self, request):
         return False
