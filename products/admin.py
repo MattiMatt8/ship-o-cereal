c839b719fe9032d97f90a0ac280f003b8a24e61d
MattiMatt8 <momatthiasson@gmail.com>
2021-05-13 07:09:13 +0000

    Admin product details updated

--- products/admin.py
+++ products/admin.py
@@ -1,18 +1,25 @@
 import math
 from fractions import Fraction
 from django.contrib import admin
-from products.models import Product, ProductLabel
+from products.models import Product, Image
 
 
+class ProductLabelInlineAdmin(admin.TabularInline):
+    model = Product.labels.through
+
+class ImageInlineAdmin(admin.TabularInline):
+    model = Image
 
 # TODO: Review calculated maybe check it out more?
+# TODO: Fix text on label selector
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','name','brand','active','stock')
-    fields = ('name','review_calculated','stock','price','active', 'category','brand','description','info','weight','contents')
-    readonly_fields = ('id','info','review_calculated')
+    fields = ('name','review_calculated','stock','price','active', 'category','brand','description','info','weight','contents', 'label_info')
+    readonly_fields = ('id','info','review_calculated','label_info')
     list_filter = ('brand',)
     list_editable = ('active',)
     search_fields = ('name','id','brand__name')
+    inlines = (ProductLabelInlineAdmin, ImageInlineAdmin)
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -20,6 +27,8 @@ class ProductAdmin(admin.ModelAdmin):
     def info(self, obj):
         return "If the product is not cereal please leave weight & contents fields empty."
 
+    def label_info(self, obj):
+        return "Each label can only be selected once."
 
 
 class ProductDiscountAdmin(admin.ModelAdmin):
@@ -70,8 +79,7 @@ class ProductDiscount(Product):
         proxy = True
 
 
-
+# admin.site.register(Label, LabelAdmin)
 admin.site.register(Product, ProductAdmin)
-
 admin.site.register(ProductDiscount, ProductDiscountAdmin)
 
