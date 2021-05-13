f4977b48f3b3ec5667a4caaeda456290a4667ccd
MattiMatt8 <momatthiasson@gmail.com>
2021-05-13 14:50:05 +0000

    Minor changes

--- static/js/card.js
+++ static/js/card.js
@@ -1,4 +1,6 @@
 function fixCardFormatting(e) {
+    // Formats card to a maximum of 16 digits and
+    // puts - between every four numbers.
     let new_str = "";
     let numbers = 0;
     for(let i = 0; i<e.target.value.length; i++) {
