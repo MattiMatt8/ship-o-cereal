2919a0ad9b2d9e9441a6b0a5d0a9d67c0b06df9c
MattiMatt8 <momatthiasson@gmail.com>
2021-05-13 17:52:47 +0000

    Javascript comments fixed

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -5,12 +5,16 @@ const textDisabled = document.getElementById("text-disabled");
 
 
 function enableButton() {
+  // Changes the button on the checkout page to enabled so the
+  // user is then able to continue to the next page.
   nextEnabled.classList.remove("hidden");
   nextDisabled.classList.add("hidden");
   textDisabled.classList.add("hidden");
 }
 
 function disableButton() {
+  // Changes the button on the checkout page to disabled so
+  // we can prevent the user from continuing if he hasn't completed the step.
   nextEnabled.classList.add("hidden");
   nextDisabled.classList.remove("hidden");
   textDisabled.classList.remove("hidden");
